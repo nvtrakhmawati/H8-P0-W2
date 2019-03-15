16159d803a75199ad6de0146c914349954688fb9
nvtrakhmawati <48206190+nvtrakhmawati@users.noreply.github.com>
2019-03-15 15:34:09 +0700

    Update exercises15.js

--- jumat/exercises15.js
+++ jumat/exercises15.js
@@ -1,23 +1,39 @@
 function groupAnimals(animals) {
-    var arrHasil = []
-    var indeks = 0
-    animals.sort();
-
-    for (var i=0; i<animals.length; i++){
-        if(arrHasil.length === 0){
-            arrHasil.push([animals[i]])
-        } else if(arrHasil[indeks][0][0] === animals[i][0]){
-            arrHasil[indeks].push(animals[i])
-        } else {
-            arrHasil.push([animals[i]])
-            indeks++
+    var strcmp = new Intl.Collator(undefined, {numeric:true, sensitivity:'base'}).compare;
+    for (var i=0; i<animals.length; i++) {
+        for (var j=i+1; j<animals.length; j++) {
+            check=strcmp(animals[i], animals[j]);;
+            if (check==1) {
+                var temp=animals[i];
+                animals[i]=animals[j];
+                animals[j]=temp;
+            }
         }
     }
-    return arrHasil
+    
+    var newAnimals=[];
+    
+    for (var i=0; i<animals.length; i++){
+        
+        var temp=[];
+        var charHewan= animals[i][0];
+        var hewan=animals[i];
+             
+        for(var j=animals.length-1; j>i; j--){
+          if(charHewan===animals[j][0]){
+            hewan=hewan+', '+animals[j]
+            animals.splice(j,1);
+          }
+        }
+        
+        temp.push(hewan)
+        newAnimals.push(temp)              
+    }
+    return newAnimals;                    
 }
 
 // TEST CASES
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
-// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
-// console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
-// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
+//[ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
